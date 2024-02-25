86a97f0adda35602c5446b007ab4e5d0bb4c6179
Timur Bashirov <bashirov188@gmail.com>
2024-02-25 22:54:06 +0300

    done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        int c = (9* a * a + 5 * b+ 2 + a - 7)*((a + b - 4 * a * b)/2);
+
+
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char first = 99;
+        char second = 104;
+        char third = 97;
+        char fourth = 114;
+
+        System.out.println(first);
     }
 }
